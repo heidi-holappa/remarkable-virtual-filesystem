0ed088ff4c9144c268c7116eee8245e3c6a89e54
Heidi Holappa <heidi.holappa@proton.me>
2026-02-17 19:00:58 +0200

    #8 add write to reMarkable and metadata validation
    After considering different approaches, I decided to implement write operations to reMarkable by replacing existing metadata-files. The metadata source acts now as a repository layer and carries out write operations to reMarkable device. Metadata validation happens on this layer as well.

--- src/data/remarkable_ssh_metadata_source.py
+++ src/data/remarkable_ssh_metadata_source.py
@@ -5,13 +5,18 @@
 """
 
 import subprocess
+import re
+import time
 import tarfile
 import json
 from typing import Dict, Any
 from io import BytesIO
+from collections.abc import Callable
 
 from src.data.metadata_source import MetadataSource
 from src.constant import REMOTE_PREFIX, SSH_CONNECT
+from src.exception.remarkable_write_exception import RemarkableWriteException
+from src.exception.invalid_metadata_exception import InvalidMetadataException
 
 class RemarkableSSHMetadataSource(MetadataSource):
     """
@@ -19,7 +24,15 @@ class RemarkableSSHMetadataSource(MetadataSource):
         interacting with the reMarkable device via SSH connection
     """
 
-    def load(self) -> dict[str, dict[str, Any]]:
+    _UUID_REGEX = re.compile(
+        r"^[0-9a-f]{8}-"
+        r"[0-9a-f]{4}-"
+        r"[0-9a-f]{4}-"
+        r"[0-9a-f]{4}-"
+        r"[0-9a-f]{12}$"
+    )
+
+    def load(self) -> Dict[str, Dict[str, Any]]:
         """
         A public method to load metadata into memory
         :return: a dictionary of metadata
@@ -101,8 +114,8 @@ class RemarkableSSHMetadataSource(MetadataSource):
         to provide intuition on the total disk space allocated
         to each entity, including the possible user notations.
 
-        Uses common tools such as du, awk and split found in
-        BusyBox to gather the information.
+        Uses common tools such as ``du``, ``awk`` and ``split``
+        found in BusyBox to gather the information.
 
         :return: a dictionary with entity UUIDs as keys and
                     disk space allocated to each entity in kilobytes
@@ -135,3 +148,135 @@ class RemarkableSSHMetadataSource(MetadataSource):
             sizes[uuid] = int(size)
 
         return sizes
+
+    def write_metadata_to_remarkable(self, entry_uuid: str, metadata: Dict[str, str]) -> None:
+        """Write metadata file to reMarkable
+
+        Writes the provided metadata dictionary into reMarkable user file
+        directory in a file named ``<uuid>.metadata``. A possible existing
+        file on the device is overwritten.
+
+        raises:
+          **RemarkableWriteException**: indicates an exception occurred during write operation
+          **InvalidMetadataException**: the provided metadata was invalid.
+
+        :param entry_uuid: the UUID for which a metadata-file is to be written
+        :param metadata: a dictionary containing metadata
+        :return: None
+        """
+
+        if not self.is_valid_metadata(metadata):
+            raise InvalidMetadataException(
+                f"The metadata was invalid: {metadata}"
+            )
+
+        metadata_filename = f"{entry_uuid}.metadata"
+        metadata_content = json.dumps(metadata, indent=4)
+
+        cmd = REMOTE_PREFIX + f"cat > '{metadata_filename}'"
+
+        try:
+            with subprocess.Popen(
+                    SSH_CONNECT + [cmd],
+                    stdin=subprocess.PIPE,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    text=True,
+            ) as proc:
+
+                stdout, stderr = proc.communicate(metadata_content)
+
+                if proc.returncode != 0:
+                    raise RemarkableWriteException(
+                        f"Failed to write metadata: {stderr.strip()}"
+                    )
+
+        except OSError as e:
+            raise RemarkableWriteException(
+                f"OS error while writing metadata: {e}"
+            ) from e
+
+
+    def is_valid_metadata(self, metadata: Dict[str, str]) -> bool:
+        """Validates a metadata entry
+
+        Validates that the given dictionary is a valid representation
+        of reMarkable metadata-file.
+
+        To be valid:
+          1. Each metadata field must exist and have valid data entries.
+          2. The dictionary contains no other fields.
+
+        :param metadata: a dictionary representation of a metadata entry
+        :return: boolean result for validation
+        """
+
+        valid_fields: Dict[str, Callable[[str], bool]] = {
+            "createdTime": self._is_valid_epoch_in_milliseconds_field,
+            "lastModified": self._is_valid_epoch_in_milliseconds_field,
+            "new": self._is_valid_boolean_metadata_field,
+            "parent": self._is_valid_uuid_field,
+            "pinned": self._is_valid_boolean_metadata_field,
+            "source": self._is_valid_string_field,
+            "type": self._is_valid_metadata_type_field,
+            "visibleName": self._is_valid_string_field
+        }
+
+        if set(metadata.keys()) != set(valid_fields.keys()):
+            print(f"ERROR: metadata entry has invalid keys: {metadata}")
+            return False
+
+
+        for key, validator in valid_fields.items():
+            value = metadata.get(key)
+            if value is None or not validator(value):
+                print(f"ERROR: invalid value in metadata field {key}: {value}")
+                return False
+
+        return True
+
+
+    def _is_valid_boolean_metadata_field(self, boolean_field: str) -> bool:
+        """Validates a boolean metadata field"""
+        return isinstance(boolean_field, bool)
+
+    def _is_valid_epoch_in_milliseconds_field(self, epoch_field: str) -> bool:
+        """validates given string is a valid epoch milliseconds field"""
+        if not isinstance(epoch_field, str):
+            return False
+
+        if not epoch_field.isdigit():
+            return False
+
+        # As the value is in milliseconds, we expect it to be atleast 13 digits in length
+        if len(epoch_field) < 13:
+            return False
+
+        try:
+            value = int(epoch_field)
+        except ValueError:
+            return False
+
+        # crudely evaluate the value is reasonable
+        now_ms = int(time.time() * 1000)
+        if value < 0 or value > now_ms + 10_000:
+            return False
+
+        return True
+
+    def _is_valid_string_field(self, str_field: str) -> bool:
+        """validates the given field is a string field"""
+        return isinstance(str_field, str)
+
+    def _is_valid_uuid_field(self, uuid_field: str) -> bool:
+        """
+            Validates the given field is either a valid UUID field
+            or the root entry (empty string)
+        """
+        if not isinstance(uuid_field, str):
+            return False
+        return uuid_field == "" or bool(self._UUID_REGEX.match(uuid_field))
+
+    def _is_valid_metadata_type_field(self, type: str) -> bool:
+        return type in ["CollectionType", "DocumentType"]
+
