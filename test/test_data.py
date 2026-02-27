89b9e0a1d3c76880546a3c85269bb173dfe6c874
Heidi Holappa <heidi.holappa@proton.me>
2026-02-27 14:38:18 +0200

    #27 add metadata for collection with identical visibleName A

--- test/test_data.py
+++ test/test_data.py
@@ -6,12 +6,14 @@
 
 from typing import Dict
 
+UUID_ROOT = ''
 UUID_A = "f6c6fa40-a38f-4b05-a31c-2a64bd6aed70"
 UUID_A0 = "d066ef0a-e9ee-475d-a04d-c1f2be88768d"
 UUID_B = "48635ee3-f8af-4013-98f3-7f2bfc2de0e4"
 UUID_B0 = "52da29a9-96fd-47fc-bd7d-7eb825be10a3"
 UUID_FAIRYTALE = "38e9f881-9cba-400d-b238-75c3bd6d64a8"
 UUID_INVALID_LAST_MODIFIED = "5592fdc0-020c-4a69-b4d3-a3106ca328ca"
+UUID_A_UNDER_B = "b21ca949-1d94-4b61-afd8-59bcf7330721"
 
 
 TEST_DATA: Dict[str, Dict[str, str]] = {
@@ -42,4 +44,5 @@ TEST_DATA: Dict[str, Dict[str, str]] = {
             },
             UUID_B: {"type": "CollectionType", "parent": "", "visibleName": "B"},
             UUID_B0: {"type": "CollectionType", "parent": UUID_B, "visibleName": "B_0"},
+            UUID_A_UNDER_B: {"type": "CollectionType", "parent": UUID_B, "visibleName": "A"},
         }
